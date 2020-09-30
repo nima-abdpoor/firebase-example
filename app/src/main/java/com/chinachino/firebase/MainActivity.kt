8c7230b8b1680099a3e54824e407d6890a571b5c
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-09-30 16:21:42 +0330

    Authentication is not completed

--- app/src/main/java/com/chinachino/firebase/MainActivity.kt
+++ app/src/main/java/com/chinachino/firebase/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.chinachino.firebase
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+    }
+    private fun signIn(email: String, password: String){
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = mAuth!!.currentUser
+                    updateUI(user)
+                }
+                else{
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", it.exception)
+                    Toast.makeText(this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
     }
 }
